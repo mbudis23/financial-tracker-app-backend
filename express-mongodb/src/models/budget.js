e846380b7b42f143d22661c790bfd51f216deec1
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-12-26 15:05:59 +0700

    Feat: budget model mongo schmea

---
+++ express-mongodb/src/models/budget.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const BudgetSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    monthly_limit: {
+        type: Number,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Budget', BudgetSchema);
