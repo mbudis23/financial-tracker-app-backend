a723d4ad967258b16fe7a49088ab0f758a7d4ed1
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-12-27 09:54:08 +0700

    feat: add authenticate mechanism

--- express-mongodb/src/server.js
+++ express-mongodb/src/server.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
 
 const connectDB = require("./config/db");
 
 const authRoutes = require('./routes/authRoute');
 const transactionRoute = require('./routes/transactionRoute');
 const budgetRoute = require('./routes/budgetRoute');
+const { authenticate } = require('./middlewares/authMiddleware');
 
 dotenv.config();
 const app = express();
 
+app.use(bodyParser.json());
+
 connectDB;
 
 app.use('/auth', authRoutes);
-app.use('/transaction', transactionRoute);
-app.use('/budget', budgetRoute);
+app.use('/transaction', authenticate, transactionRoute);
+app.use('/budget', authenticate, budgetRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
