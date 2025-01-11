04bc11b3badc836d6f04be3e04650a3c9442c224
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-01-11 14:55:33 +0700

    Fix: installing winston

--- express-mongodb/_src/utils/logger.js
+++ express-mongodb/_src/utils/logger.js
@@ -1,18 +1,36 @@
-// const winston = require("winston");
+const winston = require("winston");
 
-const logger = {
-  log: (level, message) => {
-    const timestamp = new Date().toISOString();
-    const formattedMessage = `[${timestamp}] ${level.toUpperCase()}: ${message}`;
-    console.log(formattedMessage);
-  },
-  info: (message) => logger.log("info", message),
-  error: (message) => logger.log("error", message),
-  warn: (message) => logger.log("warn", message),
-  debug: (message) => logger.log("debug", message),
-};
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(
+      ({ timestamp, level, message }) =>
+        `[${timestamp}] ${level.toUpperCase()}: ${message}`
+    )
+  ),
+  transports: [
+    new winston.transports.Console(), // Hanya log ke konsol
+  ],
+});
 
 module.exports = logger;
+
+// // const winston = require("winston");
+
+// const logger = {
+//   log: (level, message) => {
+//     const timestamp = new Date().toISOString();
+//     const formattedMessage = `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+//     console.log(formattedMessage);
+//   },
+//   info: (message) => logger.log("info", message),
+//   error: (message) => logger.log("error", message),
+//   warn: (message) => logger.log("warn", message),
+//   debug: (message) => logger.log("debug", message),
+// };
+
+// module.exports = logger;
 
 // const logger = winston.createLogger({
 //   level: "info",
