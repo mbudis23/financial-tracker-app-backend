799ceb124e9584fd50a04491ccbc747276a4d040
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2025-01-11 14:25:12 +0700

    a

--- express-mongodb/src/utils/logger.js
+++ express-mongodb/src/utils/logger.js
@@ -1,16 +1,22 @@
-const winston = require('winston');
+const winston = require("winston");
 
 const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.printf(({ timestamp, level, message }) => `[${timestamp}] ${level.toUpperCase()}: ${message}`)
-    ),
-    transports: [
-        new winston.transports.Console(), // Log ke konsol
-        new winston.transports.File({ filename: 'logs/combined.log' }), // Semua log ke file
-        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }), // Log error ke file
-    ],
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(
+      ({ timestamp, level, message }) =>
+        `[${timestamp}] ${level.toUpperCase()}: ${message}`
+    )
+  ),
+  transports: [
+    new winston.transports.Console(), // Log ke konsol
+    new winston.transports.File({ filename: "_logs/combined.log" }), // Semua log ke file
+    new winston.transports.File({
+      filename: "_logs/error.log",
+      level: "error",
+    }), // Log error ke file
+  ],
 });
 
 module.exports = logger;
