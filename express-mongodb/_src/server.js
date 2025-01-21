0ad36b395956d3a87907fe100e1256d455f293a1
shluf <luthfisalis09@gmail.com>
2025-01-21 20:54:14 +0700

    update: frontend domain

--- express-mongodb/_src/server.js
+++ express-mongodb/_src/server.js
@@ -20,7 +20,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://fintrack-app-frontend.vercel.app"],
   credentials: true
 };
 app.use(
